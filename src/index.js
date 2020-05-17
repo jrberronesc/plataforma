ad0ce9e5048a918bd5b61d2032201de9eec0fde7
Jefferson Berrones <berronesjefferson@gmail.com>
2020-05-16 23:32:36 -0500

    Agrega Provider a nuestra app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 // NATIVO
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 // COMPONENTS
-import App from './components/App';
+import App from './components/App'
 
 // CSS
 import './styles/styles.scss' 
 
+//IMPORTACIONES ALEATORIAS
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode >
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
